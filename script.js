108f92f511b66320c5a0b9403f41ce85d1a0a841
svanderstichel <vanderstichel@gmail.com>
2025-09-20 13:59:19 -0300

    Se añadio funcionalidad de javascript.

---
+++ script.js
@@ -0,0 +1,6 @@
+function getGithub(){
+    window.open("https://github.com/svanderstichel","_blank");
+}
+function getLinkedin(){
+    window.open("https://www.linkedin.com/in/vanderstichel/","_blank");
+}
