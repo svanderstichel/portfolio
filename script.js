dee0109a5c1c50b0585c26bc17beb7057369686b
svanderstichel <vanderstichel@gmail.com>
2025-09-20 15:50:31 -0300

    Se agregó el menu de navegación de la página.

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-function getGithub(){
-    window.open("https://github.com/svanderstichel","_blank");
+function getGithub() {
+    window.open("https://github.com/svanderstichel", "_blank");
 }
-function getLinkedin(){
-    window.open("https://www.linkedin.com/in/vanderstichel/","_blank");
+function getLinkedin() {
+    window.open("https://www.linkedin.com/in/vanderstichel/", "_blank");
+}
+function getArticulos() {
+    window.open("https://github.com/svanderstichel/aplicacion-gestion-articulos", "_blank");
+}
+function getRadiacion() {
+    window.open("https://github.com/svanderstichel/prediccion-radiacion-solar", "_blank");
+}
+function getConsultorio() {
+    window.open("https://github.com/svanderstichel/consultorio-medico-MedIntegral", "_blank");
+}
+function getGreed() {
+    window.open("https://github.com/svanderstichel/juego-de-dados-Greed", "_blank");
+}
+function clickMenu() {
+    menu = document.getElementById("menu");
+    if (menu.style.display == "none" || menu.style.display == "") {
+        menu.style.display = "block";
+    } else {
+        menu.style.display = "none";
+    }
+}
+function closeMenu(){
+    menu = document.getElementById("menu");
+    menu.style.display = "none";
 }
